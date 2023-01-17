ed84e2088a7395ad1c56ebe24f3931fc2bd2e1a6
rintarokojima <rin-tennis-0515@yahoo.ne.jp>
2023-01-17 10:35:31 +0900

    flatepickr

--- resources/js/flatpickr.js
+++ resources/js/flatpickr.js
@@ -1,3 +1,26 @@
-import flatpickr from "flatpickr"; 
+import flatpickr from "flatpickr";
+import { Japanese } from "flatpickr/dist/l10n/ja.js"
 
-flatpickr("event_date", {});
+flatpickr("#event_date", {
+    "locale": Japanese ,
+    minDate: "today",
+    maxDate: new Date().fp_incr(30) 
+});
+flatpickr("#start_time", {
+    "locale": Japanese ,
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H:i",
+    time_24hr: true,
+    minTime: "16:00",
+    maxTime: "22:30",
+});
+flatpickr("#end_time", {
+    "locale": Japanese ,
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H:i",
+    time_24hr: true,
+    minTime: "16:00",
+    maxTime: "22:30",
+});
