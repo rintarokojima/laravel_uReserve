4822affbb7fa1dd5738030a1f354110ce7d25f68
rintarokojima <rin-tennis-0515@yahoo.ne.jp>
2023-01-19 14:21:06 +0900

    update

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,6 +6,8 @@ use App\Http\Requests\StoreEventRequest;
 use App\Http\Requests\UpdateEventRequest;
 use App\Models\Event;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use App\Services\EventService;
 
 class EventController extends Controller
 {
@@ -34,15 +36,32 @@ class EventController extends Controller
         return view('manager.events.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreEventRequest  $request
-     * @return \Illuminate\Http\Response
-     */
+    
     public function store(StoreEventRequest $request)
     {
-        dd($request);
+        $check = EventService::checkEventDuplication(
+            $request['event_date'],$request['start_time'],$request['end_time']);
+
+        if($check){
+            session()->flash('status', 'この時間帯は既に他の予約が存在します。');
+            return view('manager.events.create');
+        }
+
+        $startDate = EventService::joinDateAndTime($request['event_date'],$request['start_time']);
+        $endDate = EventService::joinDateAndTime($request['event_date'],$request['end_time']);         
+        
+        Event::create([
+            'name' => $request['event_name'],
+            'information' => $request['information'],
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'max_people' => $request['max_people'],
+            'is_visible' => $request['is_visible']
+        ]);
+
+        session()->flash('status', '登録okです');
+
+        return to_route('events.index');
     }
 
     /**
@@ -53,7 +72,14 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        //
+        $event = Event::findOrFail($event->id);
+        $eventDate = $event->eventDate;
+        $startTime = $event->startTime;
+        $endTime = $event->endTime;
+
+        // dd($eventDate,$startTime,$endTime);
+
+        return view('manager.events.show',compact('event','eventDate','startTime','endTime'));
     }
 
     /**
@@ -64,27 +90,46 @@ class EventController extends Controller
      */
     public function edit(Event $event)
     {
-        //
+        $event = Event::findOrFail($event->id);
+        $eventDate = $event->editEventDate;
+        $startTime = $event->startTime;
+        $endTime = $event->endTime;
+
+
+        return view('manager.events.edit',compact('event','eventDate','startTime','endTime'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateEventRequest  $request
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(UpdateEventRequest $request, Event $event)
     {
-        //
+        $check = EventService::checkEventDuplication(
+            $request['event_date'],$request['start_time'],$request['end_time']);
+
+        if($check){
+            session()->flash('status', 'この時間帯は既に他の予約が存在します。');
+            return view('manager.events.edit');
+        }
+
+        $startDate = EventService::joinDateAndTime($request['event_date'],$request['start_time']);
+        $endDate = EventService::joinDateAndTime($request['event_date'],$request['end_time']);         
+        
+        $event = Event::findOrFail($event->id);
+        
+        $event->name = $request['event_name'];
+        $event->information = $request['information'];
+        $event->start_date =  $startDate;
+        $event->end_date = $endDate;
+        $event->max_people = $request['max_people'];
+        $event->is_visible = $request['is_visible'];
+        $event->save();
+        
+
+        session()->flash('status', '更新しました。');
+
+        return to_route('events.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy(Event $event)
     {
         //
